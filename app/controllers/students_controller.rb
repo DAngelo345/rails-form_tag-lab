d2aba9609e6efecfe41e826013c64c6e394a3500
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-06-09 21:31:12 -0400

    commit tree 2090b4b01789c80133d9b4617e7b4fdfea9abc2e

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,25 @@
+require 'pry'
+
 class StudentsController < ApplicationController
   
   def index
     @students = Student.all
   end
 
+  def new
+    # session[:params] = params.inspect
+    # redirect_to new_student_path
+  end
+
   def show
     @student = Student.find(params[:id])
   end
+
+  def create
+    Post.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    # session[:params] = params.inspect
+    redirect_to new_student_path
+  end 
+
 
 end
